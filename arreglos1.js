010165091d5036c44270ea40a08da3c64327f700
Dilan Solorzano <230013094+programador428-gif@users.noreply.github.com>
2026-04-28 09:51:40 -0500

    feat: funcion agregarEdad integrada

--- arreglos1.js
+++ arreglos1.js
@@ -1,2 +1,15 @@
 let edadesIzquierda = [];
 let edadesDerecha = [];
+
+function agregarEdad() {
+  let edad = parseInt(document.getElementById("edad").value);
+
+  if (!isNaN(edad)) {
+    edadesIzquierda.push(edad);
+    pintarArregloIzquierdo();
+  }
+  else {
+    alert('Por favor ingrese un numero valido');
+    return;
+  }
+}
