a251594b88112e83254c40e00c3837ad989e7fc3
Dilan Solorzano <230013094+programador428-gif@users.noreply.github.com>
2026-04-28 09:54:53 -0500

    feat: funcion para pintar el arreglo en la tabla derecha integrada

--- arreglos1.js
+++ arreglos1.js
@@ -37,6 +37,30 @@ function pintarArregloIzquierdo() {
   tbody.innerHTML = contenidodTabla;
 }
 
+function pintarArregloDerecha() {
+  let tbody = document.getElementById('tablaDerecha');
+  let contenidodTabla = '';
+  let edadRecuperada;
+
+  for (let i = 0; i < edadesDerecha.length; i++) {
+    edadRecuperada = edadesDerecha[i];
+    contenidodTabla += `
+      <tr>
+        <td>
+          <button class="btn-mover" onclick="moverHaciaIzquierda(${i}, ${edadRecuperada})">⬅</button>
+        </td>
+        <td>
+          ${edadRecuperada}
+        </td>
+        <td>
+          <button class="btn-eliminar" onclick="eliminarDerecha(${i})">Eliminar</ button>
+        </td>
+      </tr>
+    `
+  }
+  tbody.innerHTML = contenidodTabla;
+}
+
 // Funciones para eliminar
 function eliminarIzquierdo(indice) {
   edadesIzquierda.splice(indice, 1);
